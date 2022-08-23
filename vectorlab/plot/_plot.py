f9b485228e70e940a66a2b3f919106bc19a06dce
kyle.yang <kyle.yang1995@gmail.com>
2022-08-23 15:21:06 +0800

    FEAT: add networkx plot function

--- vectorlab/plot/_plot.py
+++ vectorlab/plot/_plot.py
@@ -3,8 +3,11 @@ The functions are focused on plotting relationship between variables.
 """
 
 import numpy as np
+import networkx as nx
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+from scipy import sparse
 
 from ._palettes import load_palette
 from ..utils._check import _check_ndarray
@@ -306,6 +309,171 @@ def plot3d(x, y, z, categories,
             color=colors[cat],
             label=cat
         )
+
+    if legendary:
+        ax.legend(markerscale=4, title=legend_title)
+
+    return ax
+
+
+def plotnx(adj_mat, categories,
+           cats=None, G=None,
+           ax_pos=None,
+           title=None, caption=None,
+           pos=None, with_node_labels=False,
+           arrows=False, arrowstyle='-|>', arrowsize=10,
+           edge_width=0.1, edge_attr=None, edge_font_size=5,
+           with_edge_labels=False,
+           markers='o', marker_sizes=1,
+           legendary=True, legend_title='None',
+           palette='default'):
+    r"""This will plot a network graph as desired.
+    It will first retrieve each category inside categories,
+    if cats is specified, the cats will be used instead.
+
+    Parameters
+    ----------
+    adj_mat : array_like, scipy.sparse.spmatrix, shape (n_nodes, n_nodes)
+        The adjacency matrix of a graph.
+    categories : array_like, shape (n_nodes)
+        Categories for all nodes.
+    cats : list, optional
+        The cats used to plot, this can make assign different node size
+        and node color in order.
+    ax_pos : tuple, optional
+        The positions to plot graph, provided if subplot is desired.
+    G : networkx.Graph, optional
+        The networkx.Graph to provide when adj_mat is not available.
+    title : str, optional
+        The title of plot axes.
+    caption : str, optional
+        The caption of plot axes.
+    pos : dictionary, optional
+        A dictionary with nodes as keys and positions as values.
+        If not specified a spring layout positioning will be computed.
+    with_node_labels : bool, optional
+        When set to True, nodes labels will be plotted.
+    arrows : bool, optional
+        For directed graph, if True draw arrow heads.
+    arrowstyle : str, optional
+        For directed graph, choose the style of arrow heads.
+    arrowsize : int, optional
+        For directed graph, choose the size of the arrow heads.
+    edge_width : float, optional
+        The line width of edges.
+    edge_attr : str, optional
+        The edge attribute to be plotted on edges.
+    edge_font_size : int, optional
+        The size of edge attribute to be plotted on edges.
+    with_edge_labels : bool, optional
+        When set to True, edge labels will be plotted.
+    markers : str, optional
+        The shape of the nodes.
+    marker_sizes : int or array_like, shape (n_nodes) , optional
+        The size of the nodes, it can be a fixed integer or a list to specify
+        every node.
+    palette : str, optional
+        The palette name used to assign difference color to
+        different category.
+    legendary : bool, optional
+        Whether plot legend information.
+    legend_title : str, optional
+        The legend title used to specify legend,
+        when legendary is True.
+
+    Returns
+    -------
+    ax : matplotlib.axes
+        The axes of plotting.
+
+    Raises
+    ------
+    ValueError
+        When adj_mat is not one of np.ndarray or scipy.sparse.spmatrix,
+        and meanwhile G is not provided, a ValueError is raised.
+    """
+
+    if ax_pos is not None:
+        ax = plt.subplot(*ax_pos)
+    else:
+        ax = plt.subplot(*(1, 1, 1))
+
+    if title:
+        ax.set_title(label=title, fontdict={'size': 12})
+
+    if caption:
+        ax.text(
+            s='\n' + caption,
+            horizontalalignment='left',
+            x=0, y=0,
+            fontdict={'size': 8}
+        )
+
+    if isinstance(adj_mat, np.ndarray):
+        G = nx.from_numpy_matrix(adj_mat)
+    elif sparse.isspmatrix(adj_mat):
+        G = nx.from_scipy_sparse_matrix(adj_mat)
+    else:
+        if G is None:
+            raise ValueError(
+                f'Currently adj_mat should be either np.ndarray '
+                f'or scipy.sparse.spmatrix, your type is {type(adj_mat)}. '
+                f'You can also directly input a networkx graph '
+                f'using G parameter.'
+            )
+
+    if cats is None:
+        cats, indices = np.unique(categories, return_index=True)
+        cats = cats[np.argsort(indices)]
+
+    if pos is None:
+        pos = nx.spring_layout(G, seed=42)
+
+    if not isinstance(marker_sizes, dict):
+        marker_sizes = {cat: marker_sizes for cat in cats}
+
+    node_size = np.empty(len(G), dtype=np.int_)
+    for cat in cats:
+        node_size[categories == cat] = marker_sizes[cat]
+
+    palette = load_palette(palette)
+    if np.issubdtype(cats.dtype, np.integer):
+        colors = {
+            cat: palette[cat % len(palette)]
+            for cat in cats
+        }
+    else:
+        colors = {
+            cat: palette[idx % len(palette)]
+            for idx, cat in enumerate(cats)
+        }
+
+    node_color = np.array([(0.0, 0.0, 0.0)] * len(G))
+    for cat in cats:
+        node_color[categories == cat] = colors[cat]
+
+    nx.draw_networkx(
+        G,
+        pos=pos, with_labels=with_node_labels,
+        arrows=arrows, arrowstyle=arrowstyle, arrowsize=arrowsize,
+        width=edge_width,
+        node_shape=markers, node_size=node_size, node_color=node_color
+    )
+
+    if with_edge_labels:
+        if edge_attr is None:
+            nx.draw_networkx_edge_labels(
+                G,
+                pos=pos,
+                font_size=edge_font_size
+            )
+        else:
+            nx.draw_networkx_edge_labels(
+                G,
+                pos=pos,
+                edge_labels=nx.get_edge_attributes(G, edge_attr),
+                font_size=edge_font_size
+            )
 
     if legendary:
         ax.legend(markerscale=4, title=legend_title)

df186b0be8fb04d5fab9e2babd9fbb2874e76de8
kyle.yang <kyle.yang1995@gmail.com>
2022-09-01 11:39:52 +0800

    ENH: make DegGraph in sklearn Transformer format

--- vectorlab/stats/_deg_graph.py
+++ vectorlab/stats/_deg_graph.py
@@ -8,11 +8,13 @@ import numpy as np
 
 from tqdm.auto import tqdm
 from collections import defaultdict
+from sklearn.base import TransformerMixin
 
-from ..base import SLMixin, KVNode
+from ..base import KVNode
+from ..utils._check import check_valid_int, check_pairwise_1d_array
 
 
-class DegreeGraph(SLMixin):
+class DegreeGraph(TransformerMixin):
     r"""A graph based structure that every node representing the same
     annotation on the same level.
 
@@ -35,9 +37,31 @@ class DegreeGraph(SLMixin):
         The dictionary representation of a graph.
     entrance_nodes_ : np.ndarray, shape (m)
         The entrance node to the DegreeGraph.
+    split_token_ : str
+        The default token used to split the target into chunks of tokens.
+    wild_token_ : str
+        The wild token used to replace the merged tokens.
+    deg_threshold_ : int, optional
+            The threshold that nodes' variety over this threshold will
+            be merged.
+    failed_safe_ : str, optional
+        The failed safe string used to replace the input which
+        cannot be encoded.
+    X_ : array_like, shape (n_samples)
+        The input array.
+    transformed_X_ : array_like, shape (n_samples)
+        The transformed array.
     """
 
-    def __init__(self):
+    def __init__(self, split_token='', wild_token='*',
+                 deg_threshold=10,
+                 failed_safe='invalid'):
+
+        deg_threshold = check_valid_int(
+            deg_threshold,
+            lower=1,
+            variable_name='degree threshold'
+        )
 
         self.nodes_ = np.empty(0, dtype=np.object_)
         self.layers_ = np.empty(0, dtype=np.int_)
@@ -45,6 +69,11 @@ class DegreeGraph(SLMixin):
         self.graph_dict_ = defaultdict(list)
 
         self.entrance_nodes_ = np.empty(0, dtype=np.object_)
+
+        self.split_token_ = split_token
+        self.wild_token_ = wild_token
+        self.deg_threshold_ = deg_threshold
+        self.failed_safe_ = failed_safe
 
         return
 
@@ -59,7 +88,7 @@ class DegreeGraph(SLMixin):
 
         return
 
-    def build_graph(self, targets, split_token=''):
+    def build_graph(self, targets):
         r"""Build degree graph from multiple targets.
 
         To build a degree graph, you also need to specify the split token
@@ -80,7 +109,7 @@ class DegreeGraph(SLMixin):
         edges = []
 
         for target in targets:
-            fields = target.strip().split(split_token) + ['<END>']
+            fields = target.strip().split(self.split_token_) + ['<END>']
 
             pre_node = None
             for layer, field in enumerate(fields):
@@ -122,7 +151,7 @@ class DegreeGraph(SLMixin):
 
         return
 
-    def merge_nodes(self, deg_threshold=10, verbose=False):
+    def merge_nodes(self, verbose=False):
         r"""Merge multiple nodes from the same layer.
 
         We try to merge the nodes based on the degree threshold. The merged
@@ -132,8 +161,8 @@ class DegreeGraph(SLMixin):
         the tree have the large width.
 
         When merging nodes have multiple names, we use a wild character
-        \* as the name of the new node. If there already exists a wild
-        character \* on the same level, the node will be merged into it.
+        as the name of the new node. If there already exists a wild
+        character on the same level, the node will be merged into it.
 
         Parameters
         ----------
@@ -155,7 +184,7 @@ class DegreeGraph(SLMixin):
 
             for start_node in tqdm(
                 start_nodes,
-                desc='Processing Layer {}'.format(start_layer),
+                desc=f'Processing Layer {start_layer}',
                 total=start_nodes.shape[0],
                 ascii=True,
                 disable=not verbose
@@ -168,7 +197,9 @@ class DegreeGraph(SLMixin):
                     return_counts=True
                 )
 
-                for end_node in end_nodes[end_nodes_cnts >= deg_threshold]:
+                for end_node in end_nodes[
+                    end_nodes_cnts >= self.deg_threshold_
+                ]:
 
                     common_inter_nodes = self.edges_[0][
                         (
@@ -178,14 +209,14 @@ class DegreeGraph(SLMixin):
                         )
                     ]
 
-                    if KVNode('*', inter_layer) not in self.nodes_:
+                    if KVNode(self.wild_token_, inter_layer) not in self.nodes_:
                         self.nodes_ = np.concatenate((
                             self.nodes_,
-                            [KVNode('*', inter_layer)]
+                            [KVNode(self.wild_token_, inter_layer)]
                         ))
 
                     wild_character_node = np.where(
-                        self.nodes_ == KVNode('*', inter_layer)
+                        self.nodes_ == KVNode(self.wild_token_, inter_layer)
                     )[0][0]
 
                     upper = self.edges_[
@@ -210,7 +241,8 @@ class DegreeGraph(SLMixin):
                                     ~np.isin(
                                         lower[1],
                                         end_nodes[
-                                            end_nodes_cnts >= deg_threshold
+                                            end_nodes_cnts >=
+                                            self.deg_threshold_
                                         ]
                                     )
                                 ][0]
@@ -252,9 +284,46 @@ class DegreeGraph(SLMixin):
                     self.edges_ = np.unique(self.edges_, axis=1)
 
             self._update_layers()
+
         self._gen_graph_dict()
 
         return
+
+    def fit(self, X, Y=None, verbose=False):
+        r"""Fit a DegGraph
+
+        All the input data is provided by X, while Y is set to None
+        to be ignored. In DegGraph, this function will copy the input X
+        as the attribute and fit the DegGraph.
+
+        It is a combination of operations of `build_graph` and `merge_nodes`.
+
+        Parameters
+        ----------
+        X : array_like, shape (n_samples)
+            The input array.
+        Y : Ignored
+            Not used, present for scikit-learn API consistency by convention.
+        verbose : bool, optional
+           Since the merging nodes inside DegreeGraph could be time
+            consuming, when using verbose optional, a progress bar would
+            show.
+
+        Returns
+        -------
+        self : object
+            DegGraph class object itself.
+        """
+
+        X, Y = check_pairwise_1d_array(X, Y, dtype=np.str_)
+
+        self.X_ = X
+
+        # DegGraph fit process
+        self.build_graph(self.X_)
+        self.merge_nodes(verbose=verbose)
+
+        return self
 
     def _encode(self, graph_dict, reachable_nodes,
                 fields, layer, encoded_fields,
@@ -296,69 +365,98 @@ class DegreeGraph(SLMixin):
 
         if KVNode(fields[layer], layer) in reachable_nodes:
             encoded_fields.append(fields[layer])
-            is_found = self._encode(graph_dict,
-                                    graph_dict[KVNode(fields[layer], layer)],
-                                    fields, layer + 1, encoded_fields,
-                                    is_found)
+            is_found = self._encode(
+                graph_dict, graph_dict[KVNode(fields[layer], layer)],
+                fields, layer + 1, encoded_fields,
+                is_found
+            )
 
             if not is_found:
                 encoded_fields.pop()
 
-        if not is_found and KVNode('*', layer) in reachable_nodes:
-            encoded_fields.append('*')
-            is_found = self._encode(graph_dict,
-                                    graph_dict[KVNode('*', layer)],
-                                    fields, layer + 1, encoded_fields,
-                                    is_found)
+        if not is_found and KVNode(self.wild_token_, layer) in reachable_nodes:
+            encoded_fields.append(self.wild_token_)
+            is_found = self._encode(
+                graph_dict, graph_dict[KVNode(self.wild_token_, layer)],
+                fields, layer + 1, encoded_fields,
+                is_found
+            )
 
             if not is_found:
                 encoded_fields.pop()
 
         return is_found
 
-    def encode(self, targets,
-               split_token='',
-               failed_safe='invalid'):
-        r"""Encode the input targets into a more general form.
+    def _transform_one(self, target):
+        r"""Transform one using fitted DegGraph
 
-        After the construction of a DegreeGraph, since we have merged
-        a lot of nodes with the degree over the degree threshold and
-        replace them with a wild character \*. Therefore, we also
+        After the construction of a DegGraph, since we have merged
+        a lot of nodes with the degree over the threshold and
+        replace them with a wild character. Therefore, we also
         want to encode the input that replace the merged tokens with
-        \* accordingly. This encode function will serve this purpose.
+        accordingly. This transform function will serve this purpose.
 
         Parameters
         ----------
-        targets : list
-            The list of inputs that need to be encoded.
-        split_token : str, optional
-            The split token used to split the input.
-        failed_safe : str, optional
-            The failed safe string used to replace the input which
-            cannot be encoded.
+        target : str
+            The input target.
 
         Returns
         -------
-        list
-            The list of encoded inputs.
+        transformed_target : str
+            The transformed target.
         """
 
-        encoded_targets = []
+        transformed_target = None
 
-        for target in targets:
+        layer = 0
+        reachable_nodes = self.entrance_nodes_
 
-            layer = 0
-            reachable_nodes = self.entrance_nodes_
+        encoded_fields = []
+        fields = target.strip().split(self.split_token_)
 
-            encoded_fields = []
-            fields = target.strip().split(split_token)
+        is_found = self._encode(
+            self.graph_dict_, reachable_nodes,
+            fields, layer, encoded_fields
+        )
 
-            is_found = self._encode(self.graph_dict_, reachable_nodes,
-                                    fields, layer, encoded_fields)
+        if is_found:
+            transformed_target = self.split_token_.join(encoded_fields)
+        else:
+            transformed_target = self.failed_safe_
 
-            if is_found:
-                encoded_targets.append(split_token.join(encoded_fields))
-            else:
-                encoded_targets.append(failed_safe)
+        return transformed_target
 
-        return encoded_targets
+    def transform(self, X, Y=None):
+        r"""Transform using fitted DegGraph
+
+        All the input data is provided by X, while Y is set to None
+        to be ignored. In DegGraph, this function actually transform
+        the input data X to transfromed_X.
+
+        After the construction of a DegGraph, since we have merged
+        a lot of nodes with the degree over the threshold and
+        replace them with a wild character. Therefore, we also
+        want to encode the input that replace the merged tokens with
+        accordingly. This transform function will serve this purpose.
+
+        Parameters
+        ----------
+        X : array_like, shape (n_samples)
+            The input array.
+        Y : Ignored
+            Not used, present for scikit-learn API consistency by convention.
+
+        Returns
+        -------
+        transformed_X : array_like, shape (n_samples)
+            The transformed input array.
+        """
+
+        X, Y = check_pairwise_1d_array(X, Y, dtype=np.str_)
+
+        self.transformed_X_ = np.vectorize(
+            self._transform_one, otypes=[np.str_]
+        )(X)
+
+        return self.transformed_X_

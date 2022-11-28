fdf089b424140c78a6bded7216fc935b2cf472f3
kyle.yang <kyle.yang1995@gmail.com>
2022-11-28 18:42:56 +0800

    FEAT: add KL loss to a std normal distribution

---
+++ vectorlab/nn/modules/loss.py
@@ -0,0 +1,40 @@
+import torch
+
+from .. import functional as F
+
+
+class KLWithStdNormLoss(torch.nn.modules.loss._Loss):
+    r"""The Kullback-Leibler divergence loss to a standard normal
+    distribution.
+
+    Parameters
+    ----------
+    reduction : str, optional
+        Specifies the reduction to apply to the output.
+    """
+
+    def __init__(self, reduction='mean'):
+
+        super().__init__(reduction=reduction)
+
+        return
+
+    def forward(self, mu, logstd):
+        r"""The forward process to obtain output samples.
+
+        Parameters
+        ----------
+        mu : tensor
+            The mean of samples.
+        logstd : tensor
+            The log standard deviation of samples.
+
+        Returns
+        -------
+        loss : tensor
+            The KL loss to a standard normal distribution.
+        """
+
+        loss = F.kl_with_std_norm(mu, logstd, self.reduction)
+
+        return loss

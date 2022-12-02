7cb0202e64f3d06fb38cc81216850263b5873f35
kyle.yang <kyle.yang1995@gmail.com>
2022-12-02 14:26:57 +0800

    FEAT: add seq2seq model structure

--- vectorlab/nn/models/__init__.py
+++ vectorlab/nn/models/__init__.py
@@ -1,6 +1,6 @@
 from ._mlp import *
-
-from ._classifier import *
-from ._autoencoder import *
 from ._encoder import *
 from ._decoder import *
+from ._classifier import *
+from ._autoencoder import *
+from ._seq2seq import *

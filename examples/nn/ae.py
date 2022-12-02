ffe1967c764722221776f7f603bce3e91ddc46ff
kyle.yang <kyle.yang1995@gmail.com>
2022-12-02 16:33:03 +0800

    FEAT: add nn examples

---
+++ examples/nn/ae.py
@@ -0,0 +1,68 @@
+import torch
+import vectorlab
+
+x = torch.rand(100, 3)
+dataset = torch.utils.data.dataset.TensorDataset(x)
+
+seq = torch.rand(100, 10, 3)
+seq_dataset = vectorlab.data.dataset.SequenceDataset(seq, seq)
+
+# ============= AE ==============
+ae_net = vectorlab.nn.models.AE(
+    encoder=vectorlab.nn.models.MLPEncoder(
+        in_dims=3, hidden_dims=8, num_layers=2
+    ),
+    decoder=vectorlab.nn.models.MLPDecoder(
+        hidden_dims=8, out_dims=3, num_layers=2
+    ),
+    sigmoid=True
+)
+ae_loss_fn = torch.nn.MSELoss()
+explorer = vectorlab.nn.Explorer(
+    ae_net, ae_loss_fn,
+    batch_input='X', net_input='X', loss_input='X',
+    num_epochs=10
+)
+explorer.train(dataset, verbose=2, save_last=False)
+
+# ============= VAE ==============
+vae_net = vectorlab.nn.models.VAE(
+    encoder=vectorlab.nn.models.MLPVarEncoder(
+        in_dims=3, hidden_dims=8, num_layers=2
+    ),
+    decoder=vectorlab.nn.models.MLPDecoder(
+        hidden_dims=8, out_dims=3, num_layers=2
+    ),
+    sigmoid=True
+)
+
+
+def vae_loss_fn(y_hat, y):
+    return torch.nn.MSELoss()(y_hat, y) + vae_net.kl_loss()
+
+
+explorer = vectorlab.nn.Explorer(
+    vae_net, vae_loss_fn,
+    batch_input='X', net_input='X', loss_input='X',
+    num_epochs=10
+)
+explorer.train(dataset, verbose=2, save_last=False)
+
+# ============= RNN AE ==============
+rnn_net = vectorlab.nn.models.RNNAE(
+    encoder=vectorlab.nn.models.GRUEncoder(
+        in_dims=3, hidden_dims=8, num_layers=2
+    ),
+    decoder=vectorlab.nn.models.GRUDecoder(
+        in_dims=3, hidden_dims=8, out_dims=3, num_layers=2
+    ),
+    sigmoid=True
+)
+gru_loss_fn = torch.nn.MSELoss()
+explorer = vectorlab.nn.Explorer(
+    rnn_net, gru_loss_fn,
+    train_loader_fn='pad_seqs_dataloader',
+    batch_input='X, X_lens, y, y_lens', net_input='X', loss_input='y',
+    num_epochs=10
+)
+explorer.train(seq_dataset, verbose=2, save_last=False)
---
+++ examples/nn/seq2seq.py
@@ -0,0 +1,29 @@
+import torch
+import vectorlab
+
+x = torch.randint(0, 10, (10, 4))
+y = torch.randint(0, 20, (10, 4))
+
+dataset = vectorlab.data.dataset.SequenceDataset(x, y)
+
+net = vectorlab.nn.models.Seq2Seq(
+    encoder_embedding=torch.nn.Embedding(10, 3),
+    encoder=vectorlab.nn.models.GRUEncoder(
+        in_dims=3, hidden_dims=8, num_layers=2
+    ),
+    decoder_embedding=torch.nn.Embedding(20, 3),
+    decoder=vectorlab.nn.models.GRUDecoder(
+        in_dims=3, hidden_dims=8, out_dims=20, num_layers=2
+    ),
+    start_token=0,
+    teacher_forcing=.0
+)
+loss_fn = vectorlab.nn.SequenceNLLLoss()
+explorer = vectorlab.nn.Explorer(
+    net, loss_fn,
+    train_loader_fn='pad_seqs_dataloader',
+    train_loader_kwargs={'batch_first': True},
+    batch_input='X, X_lens, y, y_lens', net_input='X, y', loss_input='y',
+    num_epochs=10
+)
+explorer.train(dataset, verbose=2, save_last=False)

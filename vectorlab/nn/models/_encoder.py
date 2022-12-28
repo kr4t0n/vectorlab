09f61338862dce84c376e282af305dd68196ce7f
kyle.yang <kyle.yang1995@gmail.com>
2022-12-28 11:44:37 +0800

    FEAT: add GNN based variational encoder

--- vectorlab/nn/models/_encoder.py
+++ vectorlab/nn/models/_encoder.py
@@ -831,6 +831,76 @@ class _BasicVarEncoder(_BasicEncoder):
         return
 
 
+class _BasicGNNVarEncoder(_BasicVarEncoder):
+    r"""An abstract basic GNN variational encoder class.
+
+    Parameters
+    ----------
+    in_dims : int
+        The dimension of input sample.
+    hidden_dims : int
+        The dimension of hidden sample.
+    num_layers : int
+        The number of hidden layers.
+    dropout : float
+        The dropout rate as a regularization method to each hidden layer.
+    bias : bool
+        Whether the model will learn an additive bias or not.
+    kwargs : dict
+        The extra arguments passed to initialize an encoder.
+
+    Attributes
+    ----------
+    in_dims_ : int
+        The dimension of input sample.
+    hidden_dims_ : int
+        The dimension of hidden sample.
+    num_layers_ : int
+        The number of hidden layers.
+    dropout_ : float
+        The dropout rate as a regularization method to each hidden layer.
+    bias_ : bool
+        Whether the model will learn an additive bias or not.
+    kwargs_ : dict
+        The extra arguments passed to initialize an encoder.
+    encoder_ : torch.nn.Module
+        The initialized encoder.
+    mu_ : torch.nn.Linear
+        The linear layer to obtain mu value.
+    logstd_ : torch.nn.Linear
+        The linear layer to obtain logstd value.
+    """
+
+    def forward(self, x, edge_index, *args, **kwargs):
+        r"""The forward process to obtain output samples.
+
+        Parameters
+        ----------
+        x : tensor
+            The input samples.
+        edge_index : tensor
+            The input edge index.
+        args : tuple
+            The extra positional arguments for forward process.
+        kwargs : dict
+            The extra keyword arguments for forward process.
+
+        Returns
+        -------
+        mu : tensor
+            The output mu value of samples.
+        logstd : tensor
+            The output logstd value of samples.
+        """
+
+        x = self.encoder_(x, edge_index, *args, **kwargs)
+
+        mu = self.mu_(x)
+        logstd = self.logstd_(x)
+
+        return mu, logstd
+
+
 class MLPVarEncoder(_BasicVarEncoder):
     r"""An abstract basic variational encoder class.
 
@@ -913,3 +983,247 @@ class MLPVarEncoder(_BasicVarEncoder):
         logstd = self.logstd_(x)
 
         return mu, logstd
+
+
+class GCNVarEncoder(_BasicGNNVarEncoder):
+    r"""An GCNConv based variational encoder.
+
+    Parameters
+    ----------
+    in_dims : int
+        The dimension of input sample.
+    hidden_dims : int
+        The dimension of hidden sample.
+    num_layers : int
+        The number of hidden layers.
+    dropout : float
+        The dropout rate as a regularization method to each hidden layer.
+    bias : bool
+        Whether the model will learn an additive bias or not.
+    kwargs : dict
+        The extra arguments passed to initialize an encoder.
+
+    Attributes
+    ----------
+    in_dims_ : int
+        The dimension of input sample.
+    hidden_dims_ : int
+        The dimension of hidden sample.
+    num_layers_ : int
+        The number of hidden layers.
+    dropout_ : float
+        The dropout rate as a regularization method to each hidden layer.
+    bias_ : bool
+        Whether the model will learn an additive bias or not.
+    kwargs_ : dict
+        The extra arguments passed to initialize an encoder.
+    encoder_ : torch.nn.Module
+        The initialized encoder.
+    mu_ : torch.nn.Linear
+        The linear layer to obtain mu value.
+    logstd_ : torch.nn.Linear
+        The linear layer to obtain logstd value.
+    """
+
+    def _init_encoder(self):
+        r"""Initialize a GCN as the encoder.
+
+        Returns
+        -------
+        encoder : torch.nn.Module
+            The GCN based encoder.
+        """
+
+        encoder = torch_geometric.nn.models.GCN(
+            in_channels=self.in_dims_, hidden_channels=self.hidden_dims_,
+            out_channels=self.hidden_dims_,
+            num_layers=self.num_layers_,
+            dropout=self.dropout_,
+            bias=self.bias_,
+            **self.kwargs_
+        )
+
+        return encoder
+
+
+class GraphVarEncoder(_BasicGNNVarEncoder):
+    r"""An GraphConv based variational encoder.
+
+    Parameters
+    ----------
+    in_dims : int
+        The dimension of input sample.
+    hidden_dims : int
+        The dimension of hidden sample.
+    num_layers : int
+        The number of hidden layers.
+    dropout : float
+        The dropout rate as a regularization method to each hidden layer.
+    bias : bool
+        Whether the model will learn an additive bias or not.
+    kwargs : dict
+        The extra arguments passed to initialize an encoder.
+
+    Attributes
+    ----------
+    in_dims_ : int
+        The dimension of input sample.
+    hidden_dims_ : int
+        The dimension of hidden sample.
+    num_layers_ : int
+        The number of hidden layers.
+    dropout_ : float
+        The dropout rate as a regularization method to each hidden layer.
+    bias_ : bool
+        Whether the model will learn an additive bias or not.
+    kwargs_ : dict
+        The extra arguments passed to initialize an encoder.
+    encoder_ : torch.nn.Module
+        The initialized encoder.
+    mu_ : torch.nn.Linear
+        The linear layer to obtain mu value.
+    logstd_ : torch.nn.Linear
+        The linear layer to obtain logstd value.
+    """
+
+    def _init_encoder(self):
+        r"""Initialize a Graph as the encoder.
+
+        Returns
+        -------
+        encoder : torch.nn.Module
+            The Graph based encoder.
+        """
+
+        encoder = Graph(
+            in_channels=self.in_dims_, hidden_channels=self.hidden_dims_,
+            out_channels=self.hidden_dims_,
+            num_layers=self.num_layers_,
+            dropout=self.dropout_,
+            bias=self.bias_,
+            **self.kwargs_
+        )
+
+        return encoder
+
+
+class GATVarEncoder(_BasicGNNVarEncoder):
+    r"""An GATConv based variational encoder.
+
+    Parameters
+    ----------
+    in_dims : int
+        The dimension of input sample.
+    hidden_dims : int
+        The dimension of hidden sample.
+    num_layers : int
+        The number of hidden layers.
+    dropout : float
+        The dropout rate as a regularization method to each hidden layer.
+    bias : bool
+        Whether the model will learn an additive bias or not.
+    kwargs : dict
+        The extra arguments passed to initialize an encoder.
+
+    Attributes
+    ----------
+    in_dims_ : int
+        The dimension of input sample.
+    hidden_dims_ : int
+        The dimension of hidden sample.
+    num_layers_ : int
+        The number of hidden layers.
+    dropout_ : float
+        The dropout rate as a regularization method to each hidden layer.
+    bias_ : bool
+        Whether the model will learn an additive bias or not.
+    kwargs_ : dict
+        The extra arguments passed to initialize an encoder.
+    encoder_ : torch.nn.Module
+        The initialized encoder.
+    mu_ : torch.nn.Linear
+        The linear layer to obtain mu value.
+    logstd_ : torch.nn.Linear
+        The linear layer to obtain logstd value.
+    """
+
+    def _init_encoder(self):
+        r"""Initialize a GAT as the encoder.
+
+        Returns
+        -------
+        encoder : torch.nn.Module
+            The GAT based encoder.
+        """
+
+        encoder = torch_geometric.nn.models.GAT(
+            in_channels=self.in_dims_, hidden_channels=self.hidden_dims_,
+            out_channels=self.hidden_dims_,
+            num_layers=self.num_layers_,
+            dropout=self.dropout_,
+            bias=self.bias_,
+            **self.kwargs_
+        )
+
+        return encoder
+
+
+class GINVarEncoder(_BasicGNNVarEncoder):
+    r"""An GINConv based variational encoder.
+
+    Parameters
+    ----------
+    in_dims : int
+        The dimension of input sample.
+    hidden_dims : int
+        The dimension of hidden sample.
+    num_layers : int
+        The number of hidden layers.
+    dropout : float
+        The dropout rate as a regularization method to each hidden layer.
+    bias : bool
+        Whether the model will learn an additive bias or not.
+    kwargs : dict
+        The extra arguments passed to initialize an encoder.
+
+    Attributes
+    ----------
+    in_dims_ : int
+        The dimension of input sample.
+    hidden_dims_ : int
+        The dimension of hidden sample.
+    num_layers_ : int
+        The number of hidden layers.
+    dropout_ : float
+        The dropout rate as a regularization method to each hidden layer.
+    bias_ : bool
+        Whether the model will learn an additive bias or not.
+    kwargs_ : dict
+        The extra arguments passed to initialize an encoder.
+    encoder_ : torch.nn.Module
+        The initialized encoder.
+    mu_ : torch.nn.Linear
+        The linear layer to obtain mu value.
+    logstd_ : torch.nn.Linear
+        The linear layer to obtain logstd value.
+    """
+
+    def _init_encoder(self):
+        r"""Initialize a GIN as the encoder.
+
+        Returns
+        -------
+        encoder : torch.nn.Module
+            The GIN based encoder.
+        """
+
+        encoder = torch_geometric.nn.models.GIN(
+            in_channels=self.in_dims_, hidden_channels=self.hidden_dims_,
+            out_channels=self.hidden_dims_,
+            num_layers=self.num_layers_,
+            dropout=self.dropout_,
+            bias=self.bias_,
+            **self.kwargs_
+        )
+
+        return encoder

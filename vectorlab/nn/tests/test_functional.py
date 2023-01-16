2d4e45053f3bd5eceb661742f2fcf1a992f3545f
kyle.yang <kyle.yang1995@gmail.com>
2023-01-16 22:41:21 +0800

    TST: add test cases for graph recon loss function

--- vectorlab/nn/tests/test_functional.py
+++ vectorlab/nn/tests/test_functional.py
@@ -1,7 +1,7 @@
 import torch
 import pytest
 
-from vectorlab.nn.functional import kl_with_std_norm
+from vectorlab.nn.functional import kl_with_std_norm, graph_recon_loss
 
 
 @pytest.mark.parametrize('rerun', [100])
@@ -17,3 +17,17 @@ def test_kl_with_std_norm(rerun):
         l2 = torch.distributions.kl_divergence(p, q)
 
         assert torch.isclose(l1, l2)
+
+
+@pytest.mark.parametrize('rerun', [100])
+def test_graph_recon_loss(rerun):
+
+    for _ in range(rerun):
+
+        adj = torch.randint(0, 2, (10, 10))
+
+        pos_edge_index = torch.where(adj == 1)
+        neg_edge_index = torch.where(adj == 0)
+
+        l = graph_recon_loss(adj, pos_edge_index, neg_edge_index)
+        assert l.item() == 0
